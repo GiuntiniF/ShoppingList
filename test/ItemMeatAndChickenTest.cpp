ea395e53609f396f77d38c222de39f8cb4221162
GiuntiniF <francesco.giuntini2@stud.unifi.it>
2022-08-17 11:54:33 +0200

    Aggiunto Unit Testing per costruttore di ItemMeatAndChicken

---
+++ test/ItemMeatAndChickenTest.cpp
@@ -0,0 +1,15 @@
+//
+// Created by ubuntuwsl on 16/08/22.
+//
+#include "gtest/gtest.h"
+
+#include "../headers/ItemMeatAndChicken.h"
+
+TEST(ItemMeatAndChicken, DefaultConstructor) {
+ItemMeatAndChicken c;
+ASSERT_EQ("Chicken", c.getName());
+ASSERT_NE("chicken", c.getName());
+ASSERT_EQ(5, c.getBasePrice());
+ASSERT_EQ(1, c.getQuantity());
+ASSERT_EQ("Meat and Chicken", c.getCategoryName());
+}
---
+++ test/runAllTests.cpp
@@ -0,0 +1,9 @@
+//
+// Created by ubuntuwsl on 15/08/22.
+//
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
