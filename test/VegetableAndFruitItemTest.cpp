af0297c188d803cd6d926a3bb314d7f3c19298aa
GiuntiniF <francesco.giuntini2@stud.unifi.it>
2022-08-23 11:57:08 +0200

    Aggiunto codice di test VegetableAndFruitItemTest.cpp

---
+++ test/VegetableAndFruitItemTest.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ubuntuwsl on 23/08/22.
+//
+#include "gtest/gtest.h"
+
+#include "../headers/VegetableAndFruitItem.h"
+
+TEST(VegetableAndFruitItem, DefaultConstructor) {
+    VegetableAndFruitItem c;
+}
