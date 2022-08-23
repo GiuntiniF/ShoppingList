aadf4983638e8ec275e2096e2c187132aa3b195d
GiuntiniF <francesco.giuntini2@stud.unifi.it>
2022-08-23 12:22:32 +0200

    Aggiunto codice di test per construttore di default e calcolo del prezzo scontato

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "headers/Item.h"
+#include "headers/MeatAndChickenItem.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Item* turkey = new MeatAndChickenItem("Turkey", 5, 2);
+    Item* chickenError = nullptr;
+    try {
+        chickenError = new MeatAndChickenItem("chicken", 5, 2);
+    }  catch(std::invalid_argument& e) {
+        std::cerr << e.what() << std::endl;
+    }
+    if(chickenError)
+    {
+        std::cout << chickenError->getName() << std::endl;
+    }
+    std::cout << turkey->getBasePrice() << std::endl;
     return 0;
 }
