f3d0ef567ddabd6285636fe0ddb4fe1a1f3faf47
GiuntiniF <francesco.giuntini2@stud.unifi.it>
2022-08-28 16:07:07 +0200

    Agguinto Unit Testing per classe NullItem

---
+++ test/NullItemTest.cpp
@@ -0,0 +1,15 @@
+//
+// Created by ubuntuwsl on 28/08/22.
+//
+#include "gtest/gtest.h"
+
+#include "../headers/NullItem.h"
+
+TEST(NullItem, DefaultConstructor) {
+    NullItem c;
+    ASSERT_NE("Chicken", c.getName());
+    ASSERT_EQ("NULL", c.getName());
+    ASSERT_EQ(0, c.getBasePrice());
+    ASSERT_EQ(0, c.getQuantity());
+    ASSERT_EQ("", c.getCategoryName());
+}
